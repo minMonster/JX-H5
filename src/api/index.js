9607ae510d4c706d067bc1235b45ac0020d51dbb
xuan LI <lixuan@dongyin.net.cn>
2019-06-28 10:11:38 +0800

    feat: 后台地址 变更

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as auth from '@/common/auth';
 
 const service = axios.create({
-  baseURL: 'http://111.230.139.130:8090', // 请求地址
+  baseURL: 'http://122.14.208.91:8090', // 请求地址
   timeout: 20000 // request timeout
 });
 
