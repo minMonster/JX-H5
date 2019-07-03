1752cae174268762d39da4ca11df4e0b1f63b694
e <m13068735161@163.com>
2019-07-03 19:57:31 +0800

    feat: 【便民服务】页面样式完成

--- src/views/convenient-people-life/index.vue
+++ src/views/convenient-people-life/index.vue
@@ -1,19 +1,69 @@
 <!-- crated：2019-06-18  author：Monster  -->
 <template>
     <div class='convenient-people-life'>
-        便民服务
+        <img src="../../assets/convenient-people-life/banner.png" class="banner" alt="">
+        <div class="tab-container">
+            <div class="tab" v-for="(tab, index) in tabs" :key="index">
+                <img :src="tab.imgSrc" alt="" class="logo">
+                <p class="name">{{tab.name}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
   export default {
-    name: 'convenient-people-life'
+    name: 'convenient-people-life',
+    data: function () {
+      return {
+        tabs: [
+          { imgSrc: require('../../assets/convenient-people-life/logo_1_jiazheng.png'), name: '家政服务' },
+          { imgSrc: require('../../assets/convenient-people-life/logo_2_jiaoyi.png'), name: '交易市场' },
+          { imgSrc: require('../../assets/convenient-people-life/logo_3_zhaopin.png'), name: '求职招聘' },
+          { imgSrc: require('../../assets/convenient-people-life/logo_4_fangwuzulin.png'), name: '房屋租赁' },
+          { imgSrc: require('../../assets/convenient-people-life/logo_5_weizhang.png'), name: '违章查询' },
+          { imgSrc: require('../../assets/convenient-people-life/logo_6_keyun.png'), name: '客运查询' },
+          { imgSrc: require('../../assets/convenient-people-life/logo_7_zhongkao.png'), name: '中考查询' },
+          { imgSrc: require('../../assets/convenient-people-life/logo_8_chongming.png'), name: '重名查询' }
+        ]
+      }
+    }
   };
 </script>
 <style rel="stylesheet/less" lang="less">
 
     @import "../../styles/index.less";
+    @import "../../styles/variable";
 
     .convenient-people-life {
+        .banner {
+            width: 100%;
+        }
+
+        .tab-container {
+            padding: .38rem .14rem;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+
+            .tab {
+                width: 1.4rem;
+                margin: 0 .1rem .6rem;
+
+                .logo {
+                    width: 1.4rem;
+                    margin-bottom: .18rem;
+                }
+
+                .name {
+                    font-size: .24rem;
+                    font-family: @FM;
+                    font-weight: 600;
+                    color: @T1;
+                    line-height: .24rem;
+                    text-align: center;
+                }
+            }
+        }
     }
 </style>
